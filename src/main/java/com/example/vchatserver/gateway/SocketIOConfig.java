ab8a76e5da7fe9af4002f1b5c4aeb340883e4468
Даниил Макеев&Daniil Makeev <96487375+holodnayazvezda@users.noreply.github.com>
2023-06-03 18:59:03 +0300

    Commit

--- src/main/java/com/example/vchatserver/gateway/SocketIOConfig.java
+++ src/main/java/com/example/vchatserver/gateway/SocketIOConfig.java
@@ -1,17 +1,9 @@
 package com.example.vchatserver.gateway;
 
-import com.corundumstudio.socketio.AuthorizationListener;
 import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.HandshakeData;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.example.vchatserver.auth.CustomAuthenticationProvider;
-import com.sun.source.tree.Tree;
-import io.netty.handler.codec.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.Authentication;
-
-import java.util.*;
 
 @org.springframework.context.annotation.Configuration
 public class SocketIOConfig {
@@ -22,8 +14,8 @@ public class SocketIOConfig {
     public SocketIOServer socketIOServer() {
         Configuration config = new Configuration();
         config.setPort(4000);
+        config.setHostname("localhost");
         config.setAuthorizationListener(socketIoAuthorizationInterceptor);
-        SocketIOServer server = new SocketIOServer(config);
-        return server;
+        return new SocketIOServer(config);
     }
 }
